78df8130aafc0d56880581a69cba558f5e1f0d45
Simran <simran2194@gmail.com>
2017-03-25 22:42:13 +0530

    first commit

--- www/js/controllers/sport_controller.js
+++ www/js/controllers/sport_controller.js
@@ -5,8 +5,18 @@
     function ($scope, $http) {
 
       $scope.sports = [];
-      var url = 'http://www.lvh.me:8000/sports';
-      $http.get(url)
+      var url = 'http://192.168.0.54:8000/sports';
+      var req = {
+        method: 'GET',
+        url: url,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMiwiZXhwIjoxNTIxODc1MjQwLCJpc3MiOiJmcmVlaGl0IiwiYXVkIjoiY2xpZW50In0.kJbPso0eirb-I5A5qw35b_chtqyU_5KNaXu78_nfi0I',
+          'Accept': 'application/vnd.freehit.v1'
+        }
+      };
+
+      $http(req)
         .success(function(data) {
           console.log(data);
           angular.forEach(data.sports, function(value, key) {
